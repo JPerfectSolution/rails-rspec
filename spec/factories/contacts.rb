b61116a98a5732796ee0aa7fa70d4a7f4b133081
J Perfect Solution <j.pefectsolution@outlook.com>
2019-01-13 20:23:31 +0100

    complete model rspec

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :contact do
-    full_name "MyString"
-    email "MyString"
-    phone_number 1
-    address "MyText"
+    full_name     { Faker::Name.name }
+    email         { Faker::Internet.email }
+    phone_number  { Faker::PhoneNumber.phone_number }
+    address       { Faker::Address.street_address }
   end
 end
